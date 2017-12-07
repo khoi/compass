77edc8ae80ffd9e489ec73683538b328e354db04
Khoi Lai <khoi.geeky@gmail.com>
2017-12-07 16:38:12 +0700

    Document fuzzy package

--- pkg/fuzzy/match.go
+++ pkg/fuzzy/match.go
@@ -2,12 +2,16 @@ package fuzzy
 
 import "unicode"
 
+// Match fuzzily find a string in another string case sensitively
+// and returns true if there is a match
 func Match(needle, haystack string) bool {
 	return match(needle, haystack, func(r1 rune, r2 rune) bool {
 		return r1 == r2
 	})
 }
 
+// MatchFold fuzzily find a string in another string case in-sensitively
+// and returns true if there is a match
 func MatchFold(needle, haystack string) bool {
 	return match(needle, haystack, func(r1 rune, r2 rune) bool {
 		return unicode.ToLower(r1) == unicode.ToLower(r2)
