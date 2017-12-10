9c3621408af862fd5caf8b51c21f97c20f9f3adf
Khoi Lai <khoi.geeky@gmail.com>
2017-12-10 10:24:29 +0700

    Temporary implementation of cd

--- cmd/cd.go
+++ cmd/cd.go
@@ -1,7 +1,42 @@
 package cmd
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"sort"
+
+	"github.com/khoiln/sextant/pkg/database"
+	"github.com/khoiln/sextant/pkg/entry"
+	"github.com/khoiln/sextant/pkg/fuzzy"
+	"github.com/urfave/cli"
+)
 
 func CmdCd(c *cli.Context) error {
-	return cli.NewExitError("Not yet implemented", 1)
+	db := c.App.Metadata["db"].(database.DB)
+	query := c.Args().First()
+
+	entries, err := db.Read()
+
+	if err != nil {
+		return err
+	}
+
+	var filtered []*entry.Entry
+
+	if query == "" {
+		filtered = entries
+	} else {
+		for _, e := range entries {
+			if fuzzy.MatchFold(query, e.Path) {
+				filtered = append(filtered, e)
+			}
+		}
+	}
+
+	sort.Sort(entry.ByRank(filtered))
+
+	if len(filtered) > 0 {
+		fmt.Fprintf(c.App.Writer, "%s\n", filtered[len(filtered)-1].Path)
+	}
+
+	return nil
 }
---
+++ cmd/cd_test.go
@@ -0,0 +1,7 @@
+package cmd
+
+import "testing"
+
+func TestCmdCd(t *testing.T) {
+
+}
